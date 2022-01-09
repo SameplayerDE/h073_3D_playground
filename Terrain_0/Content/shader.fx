146b480988188446d60146c025c0d9c2b171122d
BuildTools <unconfigured@null.spigotmc.org>
2022-01-09 22:43:37 +0100

    changes

--- Terrain_0/Content/shader.fx
+++ Terrain_0/Content/shader.fx
@@ -9,31 +9,54 @@
 
 matrix WorldViewProjection;
 
+float Delta;
+float Total;
+
+Texture2D Texture : register(t0);
+sampler TextureSampler : register(s0)
+{
+	Texture = (Texture);
+	MinFilter = Point; // Minification Filter
+    MagFilter = Point;// Magnification Filter
+    MipFilter = Linear; // Mip-mapping
+	AddressU = Wrap; // Address Mode for U Coordinates
+	AddressV = Wrap; // Address Mode for V Coordinates
+};
+
 struct VertexShaderInput
 {
 	float4 Position : POSITION0;
-	float4 Color : COLOR0;
+	float2 TextureUVs : TEXCOORD0;
 };
 
 struct VertexShaderOutput
 {
 	float4 Position : SV_POSITION;
+	float2 TextureUVs : TEXCOORD0;
 	float4 Color : COLOR0;
 };
 
 VertexShaderOutput MainVS(in VertexShaderInput input)
 {
 	VertexShaderOutput output = (VertexShaderOutput)0;
+	
+	float4 position = input.Position;
+	float2 textureUVs = input.TextureUVs;
+	float4 color = float4(1, 1, 1, 1);
 
-	output.Position = mul(input.Position, WorldViewProjection);
-	output.Color = input.Color;
+	output.Position = mul(position, WorldViewProjection);
+	output.TextureUVs = textureUVs;
+	output.Color = color;
 
 	return output;
 }
 
 float4 MainPS(VertexShaderOutput input) : COLOR
 {
-	return input.Color;
+    float4 color = input.Color;
+    color = tex2D(TextureSampler, input.TextureUVs);
+    
+	return color;
 }
 
 technique BasicColorDrawing
@@ -42,5 +65,6 @@ technique BasicColorDrawing
 	{
 		VertexShader = compile VS_SHADERMODEL MainVS();
 		PixelShader = compile PS_SHADERMODEL MainPS();
+		CullMode = NONE;
 	}
 };
--- Terrain_0/MyGame.cs
+++ Terrain_0/MyGame.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,22 +7,45 @@ namespace Terrain_0
 {
     public class MyGame : Game0
     {
-
+        
         private Matrix _view;
         private Matrix _projection;
         private Matrix _world;
 
         private Vector3 _cameraPosition;
-        
+        private Vector3 _cameraRotation;
+        private float _fieldOfView;
+
         private VertexBuffer _vertexBuffer;
-        
+        private IndexBuffer _indexBuffer;
+
         private Effect _shader;
         private SpriteFont _font;
-        
+
+        private static readonly Random Random = new Random();
+
         public MyGame()
         {
             IsMouseVisible = true;
             Content.RootDirectory = "Content";
+
+            Window.AllowUserResizing = true;
+
+            if (GraphicsDevice == null)
+            {
+                GraphicsDeviceManager.ApplyChanges();
+            }
+
+            if (GraphicsDevice != null)
+            {
+                GraphicsDeviceManager.PreferredBackBufferWidth = GraphicsDevice.Adapter.CurrentDisplayMode.Width;
+                GraphicsDeviceManager.PreferredBackBufferHeight = GraphicsDevice.Adapter.CurrentDisplayMode.Height;
+            }
+
+            GraphicsDeviceManager.HardwareModeSwitch = false;
+            GraphicsDeviceManager.PreferMultiSampling = false;
+            GraphicsDeviceManager.IsFullScreen = true;
+            GraphicsDeviceManager.ApplyChanges();
         }
 
         protected override void LoadContent()
@@ -29,40 +53,93 @@ namespace Terrain_0
             base.LoadContent();
 
             _world = Matrix.Identity;
-            
+
+            _cameraRotation = Vector3.Zero;
+            _cameraPosition = Vector3.Zero;
+            _fieldOfView = 45f;
+
             _shader = Content.Load<Effect>("shader");
             _font = Content.Load<SpriteFont>("font");
+            
+            _shader.Parameters["Texture"]?.SetValue(Content.Load<Texture>("texture"));
 
-            var vertices = new VertexPositionColor[3];
-            
-            const float x = 0f;
-            const float y = 0f;
-            const float z = 0f;
-            
-            var vertex0 = new VertexPositionColor(new Vector3(x + 0, y + 0, z + 0), Color.White);
-            var vertex1 = new VertexPositionColor(new Vector3(x + 0, y + 1, z + 0), Color.White);
-            var vertex2 = new VertexPositionColor(new Vector3(x + 1, y + 0, z + 0), Color.White);
+            var x = 0f;
+            var y = 0f;
+            var z = 0f;
 
-            vertices[0] = vertex0;
-            vertices[1] = vertex1;
-            vertices[2] = vertex2;
-            
-            _vertexBuffer = new VertexBuffer(GraphicsDevice, VertexPositionColor.VertexDeclaration, 3, BufferUsage.WriteOnly);
+            const int width = 50;
+            const int height = 50;
+
+            var vertices = new VertexPositionTexture[width * height];
+            var indices = new short[(width - 1) * (height) * 6];
+
+            for (var i = 0; i < height; i++)
+            {
+                for (var j = 0; j < width; j++)
+                {
+                    var index = j + width * i;
+                    x = j;
+                    z = -i;
+
+                    y = Random.NextSingle() / 2f;
+
+                    //var vertex0 = new VertexPositionColor(new Vector3(x + 0, y + 0, z + 0), color);
+                    vertices[index].Position = new Vector3(x + 0, y + 0, z + 0);
+                    vertices[index].TextureCoordinate.X = x;
+                    vertices[index].TextureCoordinate.Y = -z;
+                }
+            }
+
+
+            int counter = 0;
+            for (int i = 0; i < height - 1; i++)
+            {
+                for (int j = 0; j < width - 1; j++)
+                {
+                    int lowerLeft = j + i * width;
+                    int lowerRight = (j + 1) + i * width;
+                    int topLeft = j + (i + 1) * width;
+                    int topRight = (j + 1) + (i + 1) * width;
+
+                    indices[counter++] = (short) topLeft;
+                    indices[counter++] = (short) lowerRight;
+                    indices[counter++] = (short) lowerLeft;
+
+                    indices[counter++] = (short) topLeft;
+                    indices[counter++] = (short) topRight;
+                    indices[counter++] = (short) lowerRight;
+                }
+            }
+
+            _vertexBuffer = new VertexBuffer(GraphicsDevice, VertexPositionTexture.VertexDeclaration, vertices.Length,
+                BufferUsage.WriteOnly);
             _vertexBuffer.SetData(vertices);
-            
+
+            _indexBuffer = new IndexBuffer(GraphicsDevice, IndexElementSize.SixteenBits, indices.Length,
+                BufferUsage.WriteOnly);
+            _indexBuffer.SetData(indices);
+
             GraphicsDevice.SetVertexBuffer(_vertexBuffer);
-            
+            GraphicsDevice.Indices = _indexBuffer;
         }
 
         protected override void Update(GameTime gameTime)
         {
-            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            var deltaTime = (float) gameTime.ElapsedGameTime.TotalSeconds;
+            var totalTime = (float) gameTime.TotalGameTime.TotalSeconds;
             var keyboardState = Keyboard.GetState();
 
+            const float movementSpeed = 2f;
+            const float rotationSpeed = 2f;
 
-            const float speed = 10f;
             var velocity = Vector3.Zero;
+            var rotation = Vector3.Zero;
 
+            if (keyboardState.IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+            
             if (keyboardState.IsKeyUp(Keys.LeftShift))
             {
                 //Forward
@@ -70,20 +147,35 @@ namespace Terrain_0
                 {
                     velocity.Z -= 1f;
                 }
+
                 //Right
                 if (keyboardState.IsKeyDown(Keys.Right))
                 {
                     velocity.X += 1f;
                 }
+
                 //Backwards
                 if (keyboardState.IsKeyDown(Keys.Down))
                 {
                     velocity.Z += 1f;
                 }
+
                 //Left
                 if (keyboardState.IsKeyDown(Keys.Left))
                 {
                     velocity.X -= 1f;
+                }
+
+                //Up
+                if (keyboardState.IsKeyDown(Keys.PageUp))
+                {
+                    velocity.Y += 1f;
+                }
+
+                //Down
+                if (keyboardState.IsKeyDown(Keys.PageDown))
+                {
+                    velocity.Y -= 1f;
                 }
             }
             else
@@ -91,35 +183,70 @@ namespace Terrain_0
                 //Up
                 if (keyboardState.IsKeyDown(Keys.Up))
                 {
-                    velocity.Y += 1f;
+                    rotation.X += 1f;
                 }
+
                 //Down
                 if (keyboardState.IsKeyDown(Keys.Down))
                 {
-                    velocity.Y -= 1f;
+                    rotation.X -= 1f;
                 }
+
                 //Rotate Right
                 if (keyboardState.IsKeyDown(Keys.Right))
                 {
-                    //velocity.X += 1f;
+                    rotation.Y -= 1f;
                 }
+
                 //Rotate Left
                 if (keyboardState.IsKeyDown(Keys.Left))
                 {
-                    //velocity.X -= 1f;
+                    rotation.Y += 1f;
                 }
             }
+
+            if (rotation.Length() != 0)
+            {
+                rotation.Normalize();
+                rotation *= rotationSpeed;
+                rotation *= deltaTime;
+
+                _cameraRotation += rotation;
+            }
+
 
             if (velocity.Length() != 0)
             {
                 velocity.Normalize();
-                _cameraPosition += velocity * speed * deltaTime;
+                velocity *= movementSpeed;
+                velocity *= deltaTime;
+
+                var rotationMatrix = Matrix.CreateRotationY(_cameraRotation.Y);
+
+                _cameraPosition += Vector3.Transform(velocity, rotationMatrix);
             }
-            
-            _view = Matrix.CreateLookAt(_cameraPosition, _cameraPosition + Vector3.Forward, Vector3.Up);
-            _projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(70f), GraphicsDevice.Viewport.AspectRatio, 1f, 100f);
-            
+
+            var cameraRotationMatrix = Matrix.CreateRotationX(_cameraRotation.X) *
+                                       Matrix.CreateRotationY(_cameraRotation.Y) *
+                                       Matrix.CreateRotationZ(_cameraRotation.Z);
+            var cameraDirection = Vector3.Transform(Vector3.Forward, cameraRotationMatrix);
+
+            _view = Matrix.CreateLookAt(
+                _cameraPosition,
+                _cameraPosition + cameraDirection,
+                Vector3.Up
+            );
+
+            _projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.ToRadians(_fieldOfView),
+                GraphicsDevice.Viewport.AspectRatio,
+                0.1f,
+                100f
+            );
+
             _shader.Parameters["WorldViewProjection"]?.SetValue(_world * _view * _projection);
+            _shader.Parameters["Delta"]?.SetValue(deltaTime);
+            _shader.Parameters["Total"]?.SetValue(totalTime);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -129,9 +256,8 @@ namespace Terrain_0
             foreach (var pass in _shader.CurrentTechnique.Passes)
             {
                 pass.Apply();
-                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
+                GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, _indexBuffer.IndexCount / 3);
             }
-            
         }
     }
 }
