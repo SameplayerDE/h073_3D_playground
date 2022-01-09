d8311b593e7f2ab9a041a4921aee4dc1cbd39e3b
SameplayerDE <asameplayer@gmail.com>
2022-01-09 17:38:15 +0100

    Initial commit

---
+++ h073_3D_playground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace h073_3D_playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
