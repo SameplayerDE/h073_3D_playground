25d8667cb107bac4cd7d682406dc0b563d026232
SameplayerDE <asameplayer@gmail.com>
2022-01-09 17:56:34 +0100

    init

---
+++ Terrain_0/MyGame.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Terrain_0
+{
+    public class MyGame : Game0
+    {
+        public MyGame()
+        {
+            IsMouseVisible = true;
+        }
+        
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+        }
+    }
+}
---
+++ Terrain_0/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Terrain_0
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            using var game = new MyGame();
+            game.Run();
+        }
+    }
+}
