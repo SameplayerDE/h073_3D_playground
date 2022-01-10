e7a9c1c74438748fd0cb5e8f1dee2e93c10d980e
SameplayerDE <asameplayer@gmail.com>
2022-01-10 12:42:49 +0100

    cha

---
+++ Texture_2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Texture_2
+{
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            using var game = new MyGame();
+            game.Run();
+        }
+    }
+}
---
+++ Voxel_0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Voxel_0;
+
+public static class Program
+{
+    public static void Main()
+    {
+        using var game = new MyGame();
+        game.Run();
+    }
+}
